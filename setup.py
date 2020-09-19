78d87d69dd160b5282e2cb18b8dcd177fbc526d4
Brandi Nation <brandination@Brandis-MBP.lan>
2020-09-18 20:00:21 -0500

    update bnation

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as readme_file:
 requirements = ["ipython>=6", "nbformat>=4", "nbconvert>=5", "requests>=2"]
 
 setup(
-    name="notebookc",
+    name="new2python",
     version="0.0.1",
     author="Brandi Nation",
     author_email="bnation27@gmail.com",
